4fd44ddb2ec008ae0557beabe31d5d4f5ed5dd4b
tyreepace <tyreepace@gmail.com>
2019-04-16 18:52:23 -0700

    Updates

---
+++ precache-manifest.d59b638a8956d7bbcf2551948faff2ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee9a4650ea26ab13da77",
+    "url": "/course-directory/static/js/runtime~main.c0590ec8.js"
+  },
+  {
+    "revision": "0f1cfc10728d15a884da",
+    "url": "/course-directory/static/js/main.498e1a1d.chunk.js"
+  },
+  {
+    "revision": "7b245d0dcb9321609078",
+    "url": "/course-directory/static/js/2.a60c1e80.chunk.js"
+  },
+  {
+    "revision": "0f1cfc10728d15a884da",
+    "url": "/course-directory/static/css/main.fb6a0f60.chunk.css"
+  },
+  {
+    "revision": "302feea2b69d457e16942ac5f7e080d2",
+    "url": "/course-directory/index.html"
+  }
+];
